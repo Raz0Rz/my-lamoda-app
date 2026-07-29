38bc30d7d363638786f96edba91ee19f517bf09d
Dubovoi Andrey <andrey-30-17@ya.ru>
2026-07-29 20:26:46 +0300

    Start work

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div className='helpinfo'>
+
+        </div>
+        <div className='navigation'>
+          
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
